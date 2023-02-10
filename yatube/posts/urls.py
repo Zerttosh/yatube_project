13f25572b7fd2a1926a3633e87a23f4e0cefe400
Стас Басов <stas.basov@whatever.com>
2023-02-10 03:08:13 +0300

    Add css and imgs

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name='main_page'),
     # Страница со списком сортов мороженого
-    path('groups/<slug:slug>/', views.group_posts),
-] 
+    path('group/<slug:slug>/', views.group_posts, name='group_page'),
+]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,21 @@ from django.template import loader
 
 def index(request):
     template_index = 'posts/index.html'
-    return render(request, template_index)
+    poem = ('Вчера Крокодил<br>улыбнулся так злобно,<br>Что мне до сих '
+            'пор<br>за него неудобно.<br><i>Рената Муха</i>'
+            )
+    context = {
+        'title': 'Главная страница',
+        'text': 'Это главная страница проекта Yatube',
+        'poem': poem,
+    }
+    return render(request, template_index, context)
 
 
 def group_posts(request, slug):
-    template = 'posts/groups.html'
-    return render(request, template)
+    template_group_posts = 'posts/group_list.html'
+    context = {
+        'title': 'Группы Yatube',
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template_group_posts, context)
